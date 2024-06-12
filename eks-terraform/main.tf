e0c2ab8ff19ce4801836381bd86de63ecf99afdc
sushmapriya53 <162276888+sushmapriya53@users.noreply.github.com>
2024-06-12 09:15:16 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -136,7 +136,7 @@ data "aws_security_group" "selected" {
   vpc_id = data.aws_vpc.main.id
   filter {
     name = "tag:Name"
-    values = ["devops-project-veera"]
+    values =["Jumphost-sg"]
  }
 }
 
